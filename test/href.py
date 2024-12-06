1acb585863aa029b4aaec4d135efc719b9bc10ff
Areej-Asad <143700078+Areej-Asad@users.noreply.github.com>
2024-12-07 01:24:17 +0400

    Create href.py

---
+++ test/href.py
@@ -0,0 +1,14 @@
+# Write a python program that:
+# -downloads contents of a given URL
+# -counts num of href attributes in the downloaded file
+
+# import requests
+# url = "input_url.pdf"
+# r = requests.get(url)
+# with open("example.pdf",'wb') as f:
+#  f.write(r.content)
+
+for i in my_string:
+  if i == 'href':
+    count += 1
+print(count)
